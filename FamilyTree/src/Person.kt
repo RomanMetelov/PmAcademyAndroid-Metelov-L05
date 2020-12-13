d1585bbdc32ce8b8e2e9365f86e274585ce0f1b4
Roman Metelov <rm0734861369@gmail.com>
2020-12-13 23:57:27 +0200

    files main.tk, Person.kt updated

--- FamilyTree/src/Person.kt
+++ FamilyTree/src/Person.kt
@@ -1,11 +1,16 @@
-class Person(val name: String, var age: Int,
-             val mother: Person, val father: Person,
-             val siblings: Array<Person>) {
-
-
-
+class Person(
+        val name: String, val age: Int,
+        var mother: Person? = null, var father: Person? = null,
+        var siblings: Array<Person>? = null
+        ) {
 
     fun isAdult(): Boolean {
+        if (this.age >= 18) return true
         return false
     }
+
+    fun amountOfRelatives(): Int? {
+        return 0
+    }
+
 }
--- FamilyTree/src/main.kt
+++ FamilyTree/src/main.kt
@@ -1,7 +1,26 @@
 fun main() {
-
+    println(getMe().amountOfRelatives())
 }
 
-fun getMe(): Person? {
-    return null
+fun getMe(): Person {
+    val me: Person = Person("Roma", 23)
+
+    me.mother = Person("Mother", 45)
+    me.father = Person("Father", 47)
+    me.siblings = arrayOf(Person("Sister", 25))
+
+    me.mother!!.mother = Person("Tetiana", 72)
+    me.mother!!.father = Person("Vladlen", 76)
+    me.mother!!.siblings = arrayOf(Person("Vasyl", 49))
+
+    me.father!!.mother = Person("Volodymyr", 74)
+    me.father!!.father = Person("Tamara", 73)
+    me.father!!.siblings = arrayOf(Person("Taras", 49))
+
+    me.mother!!.mother!!.siblings = arrayOf(Person("Viktor", 82))
+
+    me.father!!.mother!!.father = Person("Valentyn", 99)
+
+
+    return me
 }
